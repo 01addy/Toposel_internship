1f6ed6170c3b47036fd3bfa3f0d116cfdacec62c
Aditya Pratap Singh <132531853+01addy@users.noreply.github.com>
2025-02-14 04:55:32 +0530

    authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { sendEmail} from "../utils/sendEmail.js";
-import User from "../models/User.js";
+import bcrypt from "bcryptjs";  //Library for Hashing Password
+import jwt from "jsonwebtoken"; //Library for generating JSON Web Tokens (JWT)
+import { sendEmail} from "../utils/sendEmail.js"; //Utility function used for sending emails
+import User from "../models/User.js"; //Importing the User model
 
 // Registration of a new user
 export const registerUser = async (req, res) => {
@@ -16,6 +16,7 @@ export const registerUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
+    //User Interface
     const newUser = new User({
       username,
       email,
@@ -27,7 +28,7 @@ export const registerUser = async (req, res) => {
     });
 
     await newUser.save();
-    await sendEmail(email, "Registration Successful!!");
+    await sendEmail(email, "Registration Successful!!"); //Sending a confirmation email
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
@@ -38,22 +39,26 @@ export const registerUser = async (req, res) => {
 // Login user
 export const loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body; // Extracting email and password from request body
 
+    //Check whether user exists or not
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
+    // Comparison of entered password with hashed password stored in database
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
+    // Generating a JWT token with user ID
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
 
+    // To Send success response with token and user details (excluding password)
     res.status(200).json({
         token,
         user: {
@@ -80,15 +85,17 @@ export const searchUser = async (req, res) => {
       if (!query) {
         return res.status(400).json({ message: "Please provide a username or email" });
       }
-  
+
+      // Search for a user by email or username, excluding the password field
       const user = await User.findOne({
         $or: [{ email: query }, { username: query }],
-      }).select("-password");
+      }).select("-password"); 
   
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
-  
+
+      // Sending user details (excluding password) in response
       res.status(200).json({
         id: user._id,
         username: user.username,
