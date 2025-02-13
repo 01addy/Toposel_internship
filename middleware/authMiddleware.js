b9f2f7a86866e330f2e30bc635e07c30c471610a
Aditya Pratap Singh <132531853+01addy@users.noreply.github.com>
2025-02-14 05:09:03 +0530

    authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,11 @@
-import jwt from "jsonwebtoken";
+//The authMiddleware.js file is a middleware function that ensures only authenticated users can access protected API routes.
 
+import jwt from "jsonwebtoken";  // Library for generating JSON Web Tokens (JWT)
+
+// Middleware to authenticate users based on JWT
 const authMiddleware = (req, res, next) => {
   try {
+    // Retrieving the token from the Authorization header
     const token = req.header("Authorization");
 
     if (!token) {
@@ -10,7 +14,7 @@ const authMiddleware = (req, res, next) => {
 
     
     const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
-    console.log("Decoded Token:", decoded);
+    console.log("Decoded Token:", decoded); // Debugging log to check token payload
     req.user = { id: decoded.userId };
     
     next();
